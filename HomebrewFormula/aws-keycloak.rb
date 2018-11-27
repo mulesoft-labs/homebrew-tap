09c35bd566d343ea579f35bc63b6e2dd509eb88b
Chris Byron <chris.byron@salesforce.com>
2018-11-27 13:40:09 -0800

    bottle

--- HomebrewFormula/aws-keycloak.rb
+++ HomebrewFormula/aws-keycloak.rb
@@ -1,5 +1,3 @@
-#require "language/go"
-
 class AwsKeycloak < Formula
   desc "aws-vault like tool for Keycloak authentication"
   homepage "https://github.com/mulesoft-labs/aws-keycloak"
@@ -7,11 +5,11 @@ class AwsKeycloak < Formula
   # curl -L $url | shasum -a256
   sha256 "479ff10396627ea152aedbd90e1c510a4d7169eca56393be57a4a3167951f625"
 
-  #bottle do
-  #  root_url "https://github.com/mulesoft-labs/aws-keycloak/releases/download/v1.3.6/"
-  #  cellar :any_skip_relocation
-  #  sha256 "6f3b3600604bb1dfdff16663616ef33d7be598f4ab95e5ee040d35dc3b1a1ba5" => :high_sierra
-  #end
+  bottle do
+    root_url "https://github.com/mulesoft-labs/aws-keycloak/releases/download/v1.3.6/"
+    cellar :any_skip_relocation
+    sha256 "14ae86baf1bad713450d8b445ad132236d61591121e3e3ef49e9c0ac6ab12936" => :high_sierra
+  end
 
   depends_on "dep" => :build
   depends_on "go" => :build
@@ -21,11 +19,9 @@ class AwsKeycloak < Formula
 
     dir = buildpath/"src/github.com/mulesoft-labs/aws-keycloak"
     dir.install buildpath.children
-    #Language::Go.stage_deps resources, buildpath/"src"
     cd dir do
       system "make", "dep"
       system "make"
-      #prefix.install_metadata
       bin.install "aws-keycloak"
     end
   end
