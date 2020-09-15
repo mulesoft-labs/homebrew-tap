5e848b59803ce10042399bad3a0e9b2a3e698165
Chris Byron <chris.byron@mulesoft.com>
2020-09-15 16:54:40 -0700

    update aws-keycloak v1.7.0

--- HomebrewFormula/aws-keycloak.rb
+++ HomebrewFormula/aws-keycloak.rb
@@ -1,15 +1,15 @@
 class AwsKeycloak < Formula
   desc "aws-vault like tool for Keycloak authentication"
   homepage "https://github.com/mulesoft-labs/aws-keycloak"
-  url "https://github.com/mulesoft-labs/aws-keycloak/archive/v1.5.0.tar.gz"
+  url "https://github.com/mulesoft-labs/aws-keycloak/archive/v1.7.0.tar.gz"
   # curl -L $url | shasum -a256
-  sha256 "420ece5c0fffe2c6e9b1c6a261fd438e2b20d1c4e727253944de8a56f67a48f7"
+  sha256 "6e09cd6a7ee887d2cc4947e2de37bef5e50beda170b76909ad89456f67ca8654"
 
-  bottle do
-    root_url "https://github.com/mulesoft-labs/aws-keycloak/releases/download/v1.5.0/"
-    cellar :any_skip_relocation
-    sha256 "9bd27c1f75a6808adc5e141f841119aa4e24b07b83c79a151f958a964c1dcacf" => :mojave
-  end
+  #bottle do
+    #root_url "https://github.com/mulesoft-labs/aws-keycloak/releases/download/v1.5.0/"
+    #cellar :any_skip_relocation
+    #sha256 "9bd27c1f75a6808adc5e141f841119aa4e24b07b83c79a151f958a964c1dcacf" => :mojave
+  #end
 
   depends_on "dep" => :build
   depends_on "go" => :build
@@ -20,7 +20,6 @@ class AwsKeycloak < Formula
     dir = buildpath/"src/github.com/mulesoft-labs/aws-keycloak"
     dir.install buildpath.children
     cd dir do
-      system "make", "dep"
       system "make"
       bin.install "aws-keycloak"
     end
