76d29c2c5e4907efd4f0acfc0ade0a2dc0572513
Chris Byron <chris.byron@mulesoft.com>
2020-09-16 11:30:06 -0700

    update aws-keycloak v1.7.1

--- HomebrewFormula/aws-keycloak.rb
+++ HomebrewFormula/aws-keycloak.rb
@@ -1,16 +1,16 @@
 class AwsKeycloak < Formula
   desc "aws-vault like tool for Keycloak authentication"
   homepage "https://github.com/mulesoft-labs/aws-keycloak"
-  url "https://github.com/mulesoft-labs/aws-keycloak/archive/v1.7.0.tar.gz"
+  url "https://github.com/mulesoft-labs/aws-keycloak/archive/v1.7.1.tar.gz"
   # curl -L $url | shasum -a256
-  sha256 "6e09cd6a7ee887d2cc4947e2de37bef5e50beda170b76909ad89456f67ca8654"
+  sha256 "f4a24879573b5740101090a9252d0bc1083d11007219a3bcae93fde500168f8c"
 
-  bottle do
-    root_url "https://github.com/mulesoft-labs/aws-keycloak/releases/download/v1.7.0/"
-    cellar :any_skip_relocation
-    sha256 "c24fd6d6aab16a18947b91c47883e39e8cfda9f07be233409de2267c514bec9b" => :catalina
-    sha256 "d96959fc8c5b8b51f5019eedc9174dc21adaecc893a3b0727d1bf18e4de8ae7c" => :mojave
-  end
+  #bottle do
+    #root_url "https://github.com/mulesoft-labs/aws-keycloak/releases/download/v1.7.0/"
+    #cellar :any_skip_relocation
+    #sha256 "c24fd6d6aab16a18947b91c47883e39e8cfda9f07be233409de2267c514bec9b" => :catalina
+    #sha256 "d96959fc8c5b8b51f5019eedc9174dc21adaecc893a3b0727d1bf18e4de8ae7c" => :mojave
+  #end
 
   depends_on "dep" => :build
   depends_on "go" => :build
