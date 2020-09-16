9cf492865546eba8c5a87baadd835dd7f3db3650
Chris Byron <chris.byron@mulesoft.com>
2020-09-16 11:36:37 -0700

    aws-keycloak v1.7.1 bottle catalina

--- HomebrewFormula/aws-keycloak.rb
+++ HomebrewFormula/aws-keycloak.rb
@@ -5,12 +5,11 @@ class AwsKeycloak < Formula
   # curl -L $url | shasum -a256
   sha256 "f4a24879573b5740101090a9252d0bc1083d11007219a3bcae93fde500168f8c"
 
-  #bottle do
-    #root_url "https://github.com/mulesoft-labs/aws-keycloak/releases/download/v1.7.0/"
-    #cellar :any_skip_relocation
-    #sha256 "c24fd6d6aab16a18947b91c47883e39e8cfda9f07be233409de2267c514bec9b" => :catalina
-    #sha256 "d96959fc8c5b8b51f5019eedc9174dc21adaecc893a3b0727d1bf18e4de8ae7c" => :mojave
-  #end
+  bottle do
+    root_url "https://github.com/mulesoft-labs/aws-keycloak/releases/download/v1.7.1/"
+    cellar :any_skip_relocation
+    sha256 "fd4cfd58c7dce894e0f89fa6b60d926b71db69062f9cefead1f41ad3f5456cff" => :catalina
+  end
 
   depends_on "dep" => :build
   depends_on "go" => :build
