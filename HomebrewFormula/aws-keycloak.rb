7c77b430bec967ac0af0d86b3b077e33281e7609
Roxane Martin <roxane.a.martin@gmail.com>
2021-08-16 09:53:10 -0700

    Update aws-keycloak.rb

--- HomebrewFormula/aws-keycloak.rb
+++ HomebrewFormula/aws-keycloak.rb
@@ -5,10 +5,10 @@ class AwsKeycloak < Formula
   # curl -L $url | shasum -a256
   sha256 "814fd8d90d13565612212ee6f5c5223d8d9bd4e140e12acf11a50c3a14a92f82"
 
-  # bottle do
-  #   root_url "https://github.com/mulesoft-labs/aws-keycloak/releases/download/v1.7.3/"
-  #   sha256 cellar: :any_skip_relocation, catalina: "930fed9f18e43f883fced61099ff081eae9ee98d0844751e564d2d5cde5c0a0a"
-  # end
+  bottle do
+    root_url "https://github.com/mulesoft-labs/aws-keycloak/releases/download/v1.7.5/"
+    sha256 cellar: :any_skip_relocation, big_sur: "faef1c8637624dd0034caf9c4cad0e17b69be121c1a6f18b929972f86e00236d"
+  end
 
   depends_on "dep" => :build
   depends_on "go" => :build
